8bd61b4e665b4b571b1d144a486c395503c8990e
c1s1n <2632290288@qq.com>
2025-04-29 15:25:32 +0800

    修改了服务器端存在的一些问题

---
+++ src/udpbroadcast/server.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h> // 添加 Windows 线程库头文件
+#pragma comment(lib, "Ws2_32.lib")
+#include <string>
+#include <unordered_map>
+
+struct ClientInfo {
+    std::string ip_address;
+    int port;
+    bool is_online;
+};
+
+class Server
+{
+private:
+    SOCKET server_socket;
+    std::string Ip;
+    std::unordered_map<std::string, ClientInfo> client_map;
+    HANDLE hReceiverThread;
+    bool stop_thread = false;
+
+    void SetIP();
+    void addClient(const std::string& ip, int port, bool is_online);
+    static DWORD WINAPI ServerReceiverThread(LPVOID lpParam);
+
+public:
+    Server();
+    ~Server();
+    int StartReceiverThread();
+    int StopReceiverThread();
+    int SendOfflineMessage();
+};
