e8797cd7e44d9acfef0238d383095f9b3befcd8b
lxy <ddmagelxy@163.com>
2025-05-18 19:04:08 +0800

    小改动

--- src/Server/TcpServer.h
+++ src/Server/TcpServer.h
@@ -23,8 +23,6 @@ public:
 	~TcpServer();
 	//与客户端验证密码过程
 	void Connect(SOCKET& accept_client_Socket_);
-	//接受文件传输过程
-	void Receive(SOCKET& accept_client_Socket_);
 	//断点续传接收文件
 	int Hash_Receive_Resume(SOCKET client_sock, const std::string& filename);
 	//接受文件传输过程
@@ -35,9 +33,9 @@ public:
 	std::set<std::string> GetFilesInDirectory();
 	//将temp转为文件
 	void Change_Downlad_FileName(std::string recvfile_name);
-	//查看当前文件夹是否有传输文件名
-	bool fileExists(const std::string& filename);
 	// 生成新文件名，避免覆盖
 	std::string generateUniqueFileName(const std::string& fileName);
+	//清空客户端发来的数据
+	void discard_socket_recv_buffer(SOCKET sock);
 };
 
